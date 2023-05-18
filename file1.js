c40e6498b2e4cf1e407a118505b78bd97ffd6ae3
swati7543 <swatikumari08072005@gmail.com>
2023-05-18 23:40:26 +0530

    my address

---
+++ file1.js
@@ -0,0 +1,23 @@
+console.log( "hello world");.
+// declare  a veriable
+// var firstname ="sona";
+// let firstName ="shanu";
+// let firstName =22;
+// let samefirstName = 22;
+// console.log(firstName ,typeof firstName);
+// console.log( firstName + samefirstName)
+let mynewName ="prashant";
+console.log(mynewName,mynewName (length))
+// use a variable 
+// console.log(firstname);
+let newName ="sona";
+console.log(newName.length());
+
+let shanu = "sona"
+      
+shanu=shanu.length()
+console.log(shanu);
+
+// change value 
+firstname ="swati "
+console.log(firstname.length());
---
+++ file2.js
@@ -0,0 +1,63 @@
+// // // console.log("hello  world");a
+// // let namej =33;
+// // let new1 = namej.length
+// // console.log(namej,new1,typeof(namej))
+
+
+// let a =2
+// let b =5
+// let c = a+b
+// let d = a-b
+// let e = a*b
+// let f = a/b
+
+// console.log(a,b,c,d,e,f);
+
+let firstName ="swati";
+console.log(firstName);
+
+let a = "address:"
+let b = "S/O :"
+let c = "Shashi Nandan sahay"
+
+
+let f = "Dist :"
+let g="vaishali"
+
+let d = "post:"
+let e ="sondho "
+
+let j ="Vill :"
+let k ="sondho dih"
+
+let h ="PS:"
+let i="Goraul"
+
+
+
+
+let l ="Sub-Dist :";
+let m ="Mahua";
+
+let fullName = a;
+console.log(fullName);
+let shanu = b+" "+c;
+console.log(shanu);
+
+ 
+let babu= f +" "+g;
+ console.log(babu);
+
+ let sona = d +" "+e;
+console.log(sona);
+
+ let sk=j+" "+k;
+console.log(sk);
+
+ let pk=h+" "+i;
+ console.log(pk);
+
+
+
+let psk = l+" "+m;
+console.log(psk);
